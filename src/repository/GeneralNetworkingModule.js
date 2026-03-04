a84bead3f6516159537c86468181ef647944c526
Rasid Ekbal <rtechdevlopment123@gmail.com>
2026-03-04 14:07:34 +0530

    fix bug where on session saved token not fetching from the localstorage

--- src/repository/GeneralNetworkingModule.js
+++ src/repository/GeneralNetworkingModule.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import {serverUrl} from "../routes/Routes";
+import { getSecretTokenLocalStorage } from "../utils/localStorageUtil.js";
+import { getSecretToken } from "../utils/SessionStorageUtil.js";
 const api=axios.create({
     baseURL:serverUrl,
     headers:{
         "Content-Type":"application/json",
-        Authorization:`Bearer ${window.sessionStorage.getItem("token")}`
+        Authorization:`Bearer ${getSecretToken()!=null?getSecretToken():getSecretTokenLocalStorage()}`
     }
 });
 export default api;
